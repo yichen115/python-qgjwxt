221061120c33e50bfd1d10385dcd09b0021bfc22
yichen <1097179511@qq.com>
2020-01-28 21:18:18 +0800

    删掉一些没用的库
    简版里面不需要，完整版需要用来启动chrome之类的

--- jwxt.py
+++ jwxt.py
@@ -5,10 +5,7 @@ import getpass
 import pyfiglet
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def menu():
 	#功能菜单！
